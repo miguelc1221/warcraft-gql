7857db20369063604a3d384b2d75152f58756c4c
miguel correa <miguel.correa@workday.com>
2021-06-13 15:13:32 -0600

    change schema generate

--- src/api/schema.ts
+++ src/api/schema.ts
@@ -1,21 +1,19 @@
 import { makeSchema } from 'nexus'
-import { join } from 'path'
+import { resolve } from 'path'
 import * as types from './graphql'
 
 export const schema = makeSchema({
   types,
-  // Set to false because we generate when running npm run dev
-  // due to using tsc to compile instead of ts-node-dev
-  shouldGenerateArtifacts: !!process.env.GENERATE_ARTIFACTS,
   outputs: {
-    typegen: join(__dirname, '../../__generated/nexus.ts'),
-    schema: join(__dirname, '../../__generated/schema.graphql'),
+    typegen: resolve('__generated/nexus.ts'),
+    schema: resolve('__generated/schema.graphql'),
   },
   contextType: {
-    module: join(__dirname, `./context.ts`),
+    module: resolve('src/api/context.ts'),
     export: 'Context',
   },
   sourceTypes: {
+    headers: ['// @ts-nocheck'],
     modules: [
       {
         module: '@prisma/client',
@@ -23,5 +21,5 @@ export const schema = makeSchema({
       },
     ],
   },
-  prettierConfig: join(__dirname, '../../.prettierrc'),
+  prettierConfig: resolve('.prettierrc'),
 })
